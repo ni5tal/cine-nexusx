d85660856679aefc5d9d1993464f143e69092800
ni5tal <nistalmartin86@gmail.com>
2026-07-09 16:50:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Welcome to Cine Nexus!");
+// Smooth reveal animation
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".movie-card").forEach(card=>{
+    observer.observe(card);
+});
+
+// Hero animation
+
+window.addEventListener("load",()=>{
+
+    const hero=document.querySelector(".hero-content");
+
+    hero.style.opacity="1";
+
+    hero.style.transform="translateY(0)";
+});
