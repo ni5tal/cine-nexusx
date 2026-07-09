eb4881aa88d713cfab110c3fa232d6363b903e65
ni5tal <nistalmartin86@gmail.com>
2026-07-09 18:18:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,38 @@ window.addEventListener("load", () => {
         hero.style.transform = "translateY(0)";
     }
 });
+// ===== Movie Modal =====
+
+const modal = document.getElementById("movieModal");
+const closeBtn = document.querySelector(".close-btn");
+
+const movieCards = document.querySelectorAll(".movie-card");
+
+movieCards.forEach(card => {
+  card.addEventListener("click", () => {
+    document.getElementById("modalPoster").src =
+      "https://image.tmdb.org/t/p/w500/ombsmhYUqR4qqOLOxAyr5V8hbyv.jpg";
+
+    document.getElementById("modalTitle").textContent = "Superman (2025)";
+    document.getElementById("modalYear").textContent = "📅 2025";
+    document.getElementById("modalGenre").textContent = "🎭 Action / Sci-Fi";
+    document.getElementById("modalRating").textContent = "⭐ 9.2 / 10";
+    document.getElementById("modalDescription").textContent =
+      "A fresh take on Superman, directed by James Gunn.";
+
+    document.getElementById("trailerBtn").href =
+      "https://www.youtube.com/results?search_query=Superman+2025+Official+Trailer";
+
+    modal.style.display = "flex";
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
