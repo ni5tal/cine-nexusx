eb14979cf27893b4c09b2e1ad507c557dd345e5b
ni5tal <nistalmartin86@gmail.com>
2026-07-10 01:21:10 +0530

    Update discover.js

--- discover.js
+++ discover.js
@@ -1,2 +1,17 @@
-document.getElementById("movieGrid").innerHTML =
-"<h2 style='color:white'>JavaScript is working! 🎉</h2>";
+const API_KEY = "92eac45ee2e28ae00eabf532a9fa84de";
+
+fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`)
+  .then(response => {
+    console.log(response.status);
+    return response.json();
+  })
+  .then(data => {
+    document.getElementById("movieGrid").innerHTML =
+      "<pre style='color:white'>" +
+      JSON.stringify(data, null, 2) +
+      "</pre>";
+  })
+  .catch(error => {
+    document.getElementById("movieGrid").innerHTML =
+      "<h2 style='color:red'>Error: " + error + "</h2>";
+  });
