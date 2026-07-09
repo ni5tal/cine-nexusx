7590b7f52957ed579a1d948cc402185f22dc6f30
ni5tal <nistalmartin86@gmail.com>
2026-07-09 16:55:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-// Smooth reveal animation
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add("show");
         }
     });
 });
 
-document.querySelectorAll(".movie-card").forEach(card=>{
+document.querySelectorAll(".movie-card").forEach(card => {
     observer.observe(card);
 });
 
-// Hero animation
+window.addEventListener("load", () => {
+    const hero = document.querySelector(".hero-content");
 
-window.addEventListener("load",()=>{
-
-    const hero=document.querySelector(".hero-content");
-
-    hero.style.opacity="1";
-
-    hero.style.transform="translateY(0)";
+    if (hero) {
+        hero.style.opacity = "1";
+        hero.style.transform = "translateY(0)";
+    }
 });
